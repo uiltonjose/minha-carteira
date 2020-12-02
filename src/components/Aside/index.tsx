94e7910bedc52b2fc131d1e597779fba6f91ba4f
Uilton Santos <uilton.santos@sky.uk>
2020-12-02 20:49:56 +0000

    General layout

---
+++ src/components/Aside/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "./style";
+
+const Aside: React.FC = () => {
+  return (
+    <Container>
+      <h1>Aside</h1>;
+    </Container>
+  );
+};
+
+export default Aside;
---
+++ src/components/MainHeader/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "./style";
+
+const MainHeader: React.FC = () => {
+  return (
+    <Container>
+      <h1>MainHeader</h1>;
+    </Container>
+  );
+};
+
+export default MainHeader;
