eadbd5ef4f5aaee0d6070f4d9dd32279fb73d568
Uilton Santos <uilton.santos@sky.uk>
2020-12-03 20:25:54 +0000

    aside implementation

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,10 +1,47 @@
 import React from "react";
-import { Container } from "./style";
+import {
+  Container,
+  Header,
+  LogImg,
+  MenuContainer,
+  MenuItemLink,
+  Title,
+} from "./style";
+import {
+  MdDashboard,
+  MdArrowDownward,
+  MdArrowUpward,
+  MdExitToApp,
+} from "react-icons/md";
+
+import logoImg from "../../assets/logo.svg";
 
 const Aside: React.FC = () => {
   return (
     <Container>
-      <h1>Aside</h1>;
+      <Header>
+        <LogImg src={logoImg} alt="Logo Minha Carteira" />
+        <Title>Minha Carteira</Title>
+      </Header>
+
+      <MenuContainer>
+        <MenuItemLink href="#">
+          <MdDashboard />
+          Dashboard
+        </MenuItemLink>
+        <MenuItemLink href="#">
+          <MdArrowDownward />
+          Entradas
+        </MenuItemLink>
+        <MenuItemLink href="#">
+          <MdArrowUpward />
+          Saídas
+        </MenuItemLink>
+        <MenuItemLink href="#">
+          <MdExitToApp />
+          Sair
+        </MenuItemLink>
+      </MenuContainer>
     </Container>
   );
 };
