574dbd9fa886b0e699c0e8bb299489500ee8dba2
Uilton Santos <uilton.santos@sky.uk>
2020-12-05 21:20:25 +0000

    - custom scrollbar - list implementation

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,21 +1,47 @@
 import React from "react";
 import ContentHeader from "../../components/ContentHeader";
+import HistoryFinanceCard from "../../components/HistoryFinanceCard";
 import SelectInput from "../../components/SelectInput";
 
-import { Container } from "./styles";
+import { Container, Content, Filters } from "./styles";
 
 const List: React.FC = () => {
-  const options = [
-    { value: "Uilton", label: "Uilton" },
-    { value: "Viviane", label: "Viviane" },
-    { value: "Sofia", label: "Sofia" },
+  const months = [
+    { value: 7, label: "Julho" },
+    { value: 8, label: "Agosto" },
+    { value: 9, label: "Setembro" },
+  ];
+
+  const years = [
+    { value: 2020, label: 2020 },
+    { value: 2019, label: 2019 },
+    { value: 2018, label: 2018 },
   ];
 
   return (
     <Container>
       <ContentHeader title="Entrada" lineColor="#831e1e">
-        <SelectInput options={options} />
+        <SelectInput options={months} />
+        <SelectInput options={years} />
       </ContentHeader>
+
+      <Filters>
+        <button type="button" className="tag-filter tag-filter-recurrent">
+          Recorrentes
+        </button>
+        <button type="button" className="tag-filter tag-filter-eventual">
+          Eventuais
+        </button>
+      </Filters>
+
+      <Content>
+        <HistoryFinanceCard
+          title="Conta de luz"
+          subtitle="27/07/2020"
+          amount="R$ 140"
+          tagColor="red"
+        />
+      </Content>
     </Container>
   );
 };
--- src/utils/emojis.ts
+++ src/utils/emojis.ts
@@ -1,1 +1,2 @@
+// eslint-disable-next-line
 export default ['😍', '😏', '😎', '😜', '🧐'];
