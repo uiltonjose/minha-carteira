551ef7d82c4e242393ccb408f567b553e291c2a5
Uilton Santos <uilton.santos@sky.uk>
2020-12-06 18:17:34 +0000

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
 import React from "react";
 import { ThemeProvider } from "styled-components";
+import Routes from "./routes";
 import GlobalStyles from "./styles/GlobalStyles";
 
-import Layout from "./components/Layout";
-import List from "./pages/List";
 import dark from "./styles/themes/dark";
 
 const App: React.FC = () => {
   return (
     <ThemeProvider theme={dark}>
       <GlobalStyles />
-      <Layout>
-        <List/>
-      </Layout>
+      <Routes/>
     </ThemeProvider>
   );
 };
--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useMemo } from "react";
 import ContentHeader from "../../components/ContentHeader";
 import HistoryFinanceCard from "../../components/HistoryFinanceCard";
 import SelectInput from "../../components/SelectInput";
 
 import { Container, Content, Filters } from "./styles";
 
-const List: React.FC = () => {
+interface IRouteParams {
+  match: {
+    params: {
+      type: string;
+    };
+  };
+}
+
+const List: React.FC<IRouteParams> = ({ match }) => {
+  const { type } = match.params;
+  const contentData = useMemo(() => {
+    return type === "entry-balance"
+      ? { title: "Entradas", lineColor: "#F7931B" }
+      : { title: "Saídas", lineColor: "#E44C4E" };
+  }, [type]);
+
   const months = [
     { value: 7, label: "Julho" },
     { value: 8, label: "Agosto" },
@@ -20,7 +35,7 @@ const List: React.FC = () => {
 
   return (
     <Container>
-      <ContentHeader title="Entrada" lineColor="#831e1e">
+      <ContentHeader title={contentData.title} lineColor={contentData.lineColor}>
         <SelectInput options={months} />
         <SelectInput options={years} />
       </ContentHeader>
