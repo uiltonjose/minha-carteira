71a5edf3f98954169a44b1fb68ce75a61e95a113
Uilton Santos <uilton.santos@sky.uk>
2020-12-07 16:23:20 +0000

    - Card wallet (with animation) - message box

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,13 +1,19 @@
 import React, { useMemo, useState } from "react";
+
 import ContentHeader from "../../components/ContentHeader";
 import SelectInput from "../../components/SelectInput";
+import WalletBox from "../../components/WalletBox";
+import MessageBox from "../../components/MessageBox";
 
 import listOfMonths from "../../utils/months";
 
 import gains from "../../repositories/gains";
 import expenses from "../../repositories/expenses";
 
-import { Container } from "./styles";
+import happyImg from "../../assets/happy.svg";
+import sadImg from "../../assets/sad.svg";
+
+import { Container, Content } from "./styles";
 
 const Dashboard: React.FC = () => {
   const [monthSelected, setMonthSelected] = useState<number>(
@@ -16,7 +22,7 @@ const Dashboard: React.FC = () => {
   const [yearSelected, setYearSelected] = useState<number>(
     new Date().getFullYear()
   );
-  
+
   const months = useMemo(() => {
     return listOfMonths.map((month, index) => {
       return {
@@ -78,6 +84,39 @@ const Dashboard: React.FC = () => {
           defaultValue={yearSelected}
         />
       </ContentHeader>
+
+      <Content>
+        <WalletBox
+          title="saldo"
+          amount={150.0}
+          footerLabel="Atualizado com base nas entradas e saídas"
+          icon="dolar"
+          color="#F7931B"
+        />
+
+        <WalletBox
+          title="entradas"
+          amount={5000.0}
+          footerLabel="Atualizado com base nas entradas e saídas"
+          icon="arrowUp"
+          color="#4E41F0"
+        />
+
+        <WalletBox
+          title="saídas"
+          amount={4850.0}
+          footerLabel="Atualizado com base nas entradas e saídas"
+          icon="arrowDown"
+          color="#E44C4E"
+        />
+
+        <MessageBox
+          title="Muito bem!"
+          description="Sua carteira está positiva!"
+          footerText="Conitnue assim. Conside investir o seu saldo."
+          icon={happyImg}
+        />
+      </Content>
     </Container>
   );
 };
