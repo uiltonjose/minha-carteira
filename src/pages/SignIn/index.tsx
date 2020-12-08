772e0b881d076ccc6093502d77e98ae7c1c7ec0e
Uilton Santos <uilton.santos@sky.uk>
2020-12-08 17:36:56 +0000

    SignIn Page

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,7 +1,38 @@
-import React from 'react';
+import React from "react";
+
+import logoImg from "../../assets/logo.svg";
+import Button from "../../components/Button";
+import Input from "../../components/Input";
+
+import { Container, Logo, Form, FormTitle } from "./styles";
 
 const SignIn: React.FC = () => {
-    return <h1>SignIn</h1>
-}
+  return (
+    <Container>
+      <Logo>
+        <img src={logoImg} alt="Minha Carteira" />
+        <h2>Minha Carteira</h2>
+      </Logo>
+
+      <Form onSubmit={() => {}}>
+        <FormTitle>Entrar</FormTitle>
+
+        <Input 
+            type="email" 
+            required 
+            placeholder="e-mail" 
+        />
+        
+        <Input 
+            type="password" 
+            required 
+            placeholder="senha" 
+        />
+        
+        <Button type="submit">Acessar</Button>
+      </Form>
+    </Container>
+  );
+};
 
 export default SignIn;
